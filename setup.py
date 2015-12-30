c562de74887efd8189f821dcdb592171085301ec
haikentcode <hiteshnitj16@gmail.com>
2015-12-31 02:45:11 +0530

    halogs module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='halogs',
+      version='0.1',
+      description='Python  Application to store log',
+      url='https://github.com/haikentcode/halogs',
+      author='HITESH KUMAR REGAR (haikent)',
+      author_email='hiteshnitj16@gmail.com',
+      license='MIT',
+      packages=['halogs'],
+      zip_safe=False)
